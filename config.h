584559c1db9c5b494e2243e85c818321d18ed207
Hacker Gaucho <84669018+hackergaucho@users.noreply.github.com>
2021-06-04 19:54:44 -0300

    firefox no lugar do chromium

--- config.h
+++ config.h
@@ -27,7 +27,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ NULL,         NULL,       "Chromium",  1,          False,      -1 },
+	{ NULL,         NULL,       "firefox",  1,          False,      -1 },
 	{ NULL,         NULL,       "Atom",     2,          False,      -1 },
 	{ NULL,         NULL,       "alarm-clock-applet",      5,          False,      -1 },
 };
